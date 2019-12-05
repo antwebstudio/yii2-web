462f6219c93851ca9f7dcbcf0e2e6a93c1dfd9eb
Hui Yang <chy1988@antwebstudio.com>
2019-12-06 02:13:22 +0800

    Added Modal widget Enhanced sidenav layout

--- src/backend/views/layouts/left-sidenav.php
+++ src/backend/views/layouts/left-sidenav.php
@@ -5,15 +5,16 @@ use yii\bootstrap\Alert;
 use kartik\sidenav\SideNav;
 
 $theme = $this->theme;
+$this->params['sideNav'] = isset($this->params['sidenav']) ? $this->params['sidenav'] : $this->params['sideNav'];
 ?>
 <?php $this->beginContent('@app/views/layouts/main.php'); ?>
-<div class="layout-left-sidenav container">
+<div class="layout-left-sidenav">
 	<div class="row">
 		<div class="col-lg-2">
         <?php  
         echo SideNav::widget([
             'type' => SideNav::TYPE_DEFAULT,
-            'heading' => isset($this->params['sideNav']['heading']) ? $this->params['sideNav']['heading'] : 'SideNav',
+            'heading' => isset($this->params['sideNav']['heading']) ? $this->params['sideNav']['heading'] : null,
             'items' => isset($this->params['sideNav']['items']) ? $this->params['sideNav']['items'] : [],
             'containerOptions' => ['class' => 'side-nav']
         ]);
---
+++ src/widgets/Modal.php
@@ -0,0 +1,23 @@
+<?php
+namespace ant\widgets;
+
+use yii\helpers\Url;
+
+class Modal extends \yii\bootstrap\Modal {
+    public $url;
+
+    public function init() {
+        if (isset($this->url)) {
+            $this->clientEvents['show.bs.modal'] = new \yii\web\JsExpression('function() {
+                var url = "'.Url::to($this->url).'";
+                $modalBody = $("#' . $this->id . '").find(".modal-body");
+                $modalBody.html("<div class=\"modal-loader\"></div>");
+                $modalBody.load(url, function() { 
+                
+                });
+            }');
+        }
+        parent::init();
+        
+    }
+}
